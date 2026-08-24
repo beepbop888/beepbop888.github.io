095270e7d8aa2502bab1a12ac45d792359f8aa2c
beepbop888 <4203700@gmail.com>
2026-08-24 08:21:43 -0400

    Stop the test apps caching themselves: unregister service workers and refuse new ones

--- apps/gallery/flutter_bootstrap.js
+++ apps/gallery/flutter_bootstrap.js
@@ -10,7 +10,4 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "2050142669"
-  }
-});
+  });
