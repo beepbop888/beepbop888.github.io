1e76fc95f6bd24d07be58695b81834f87942ccf3
beepbop888 <4203700@gmail.com>
2026-08-31 08:18:28 -0400

    Publish web builds: word, dinner, pasians

--- apps/pasians/flutter_bootstrap.js
+++ apps/pasians/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3736908087"
+    serviceWorkerVersion: "2778799418"
   }
 });
