6b144c7afa517d0782d31927640ce816be7945be
beepbop888 <4203700@gmail.com>
2026-09-03 16:51:03 -0400

    Publish web builds: headache, doplaty, bolnichny, schetchiki, otpusknye

--- apps/schetchiki/flutter_bootstrap.js
+++ apps/schetchiki/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2276497383"
+    serviceWorkerVersion: "555179854"
   }
 });
