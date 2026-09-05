de041bb54e33e14a7c20ffa9896f224075bbb96c
beepbop888 <4203700@gmail.com>
2026-09-05 07:52:13 -0400

    Publish web builds: wifi, krug, stories

--- apps/stories/flutter_service_worker.js
+++ apps/stories/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "97f26a94c28da98546a7437ab32c52fa
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "0fedf139681f3266eb079235c92d32d7",
+"flutter_bootstrap.js": "ed5cc8efe5717241b336a50ece97338b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
