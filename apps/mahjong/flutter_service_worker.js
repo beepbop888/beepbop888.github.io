f51540c5bdacae905b42528a4f7a241e02203da5
beepbop888 <4203700@gmail.com>
2026-09-09 20:51:17 -0400

    Publish web builds: krug, wifi, mahjong

--- apps/mahjong/flutter_service_worker.js
+++ apps/mahjong/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bc1b4d50202bcb3d691883013a3cc298
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "7d7704854689cb7ad9b45ec150bdf8d1",
+"flutter_bootstrap.js": "4845f19da7332d59fceb82d6ec812d27",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
