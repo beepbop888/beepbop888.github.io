ce236051cd5754963efaa107d7cc964ac0998b88
beepbop888 <4203700@gmail.com>
2026-09-11 08:32:50 -0400

    Publish web builds: puriny

--- apps/puriny/flutter_service_worker.js
+++ apps/puriny/flutter_service_worker.js
@@ -33,14 +33,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a72244864b0adae2e175d767aca891ce
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "940c927e31830c2f81d2ef4b3e8adb81",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "630f4ff3cccd6d6f4c5ab9df0adebdcf",
+"flutter_bootstrap.js": "defd6873e4eabda394d53ee16aca7a1c",
 "icons/Icon-192.png": "2e815a0c41555d79a5c61d7aa52ab8e3",
 "icons/Icon-512.png": "fc2e548b15ad9ae21d42ee7734848f8a",
 "icons/Icon-maskable-192.png": "2e815a0c41555d79a5c61d7aa52ab8e3",
 "icons/Icon-maskable-512.png": "fc2e548b15ad9ae21d42ee7734848f8a",
 "index.html": "1eb2d6a3bc920cc4da2ffe998c697326",
 "/": "1eb2d6a3bc920cc4da2ffe998c697326",
-"main.dart.js": "eaf54cbefb0718dc38bf217b7cfc87b2",
+"main.dart.js": "19501213e30e84affa50c6a467100d89",
 "manifest.json": "98169aeedaf40ddc515210da07443522",
 "version.json": "56dd681b19640948cb14e06e4ba2c660"};
 // The application shell files that are downloaded before a service worker can
